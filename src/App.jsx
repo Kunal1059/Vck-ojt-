d20ff2c4e4e1cf6727f584e259ff44c5baee7fe3
Pranit03 <pranitp695@gmail.com>
2025-06-30 15:29:51 +0530

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ import DeveloperInfoPopup from "./components/DeveloperInfo/DeveloperInfoPopup";
 import ChatbotComponents from "./components/Chatbot/ChatbotComponents"
 // import ContactPage from "./pages/ContactPage";
 //import AboutPages from "./pages/AboutPage";
-import CoursesPage from "./pages/CoursesPage";
-import AdmissionsPage from "./pages/AdmissionsPage";
-import NotFoundPage from "./pages/NotFoundPage";
+//import CoursesPage from "./pages/CoursesPage";
+//import AdmissionsPage from "./pages/AdmissionsPage";
+//import NotFoundPage from "./pages/NotFoundPage";
 import HomePage from "./pages/HomePage";
 
 const App =()=> {
